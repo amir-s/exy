9be91369b8a0932b372748437894a5eac103627e
Amir Saboury <amir.saboury@gmail.com>
2016-01-10 02:18:07 -0500

    tab2space

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 var koa = require('koa'),
-	session = require('koa-generic-session'),
-	router = require('koa-router')(),
-	bodyParser = require('koa-body-parser'),
-	static = require('koa-static'),
-	send = require('koa-send');
+    session = require('koa-generic-session'),
+    router = require('koa-router')(),
+    bodyParser = require('koa-body-parser'),
+    static = require('koa-static'),
+    send = require('koa-send');
 
 
 
 router.get('/', function* (next) {
-	this.body = 'Hello World!';
+    this.body = 'Hello World!';
 });
 
 router.get('/count', function* (next) {
-	this.session.count = this.session.count || 0;
-	this.session.count++;
-	this.body = 'N = ' + this.session.count;
+    this.session.count = this.session.count || 0;
+    this.session.count++;
+    this.body = 'N = ' + this.session.count;
 });
 
 var app = new koa();
 app.keys = ['keys'];
 
 app
-	.use(static('./public/'))
-	.use(session())
-	.use(bodyParser())
-	.use(router.routes())
-	.use(router.allowedMethods())
-	
-	.listen(3000);
+    .use(static('./public/'))
+    .use(session())
+    .use(bodyParser())
+    .use(router.routes())
+    .use(router.allowedMethods())
+    
+    .listen(3000);
